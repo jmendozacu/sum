156410ac1b587fbebc7e9eae657e8383a589c41c
Eugene Polischuk <eugene.polischuk@eleanorsoft.com>
2017-09-14 14:37:12 +0300

    removed repeat password

--- app/design/frontend/Atak/summit/web/js/summit.js
+++ app/design/frontend/Atak/summit/web/js/summit.js
@@ -11,7 +11,12 @@ define([
 				$videoBlock.append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' +
 					$videoBlock.attr('data-video-id') + '?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>');
 			});
-			
+
+			if ($('body').hasClass('customer-account-create')) {
+				$('#password').blur(function() {
+					$('#password-confirmation').val($('#password').val());
+				});
+			}
 		});
 		
 		return;
