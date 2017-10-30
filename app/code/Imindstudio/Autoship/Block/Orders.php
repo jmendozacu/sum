00882328e2444e04d16df7d6fc1d6e7410bd5c7d
Eugene Polischuk <eugene.polischuk@eleanorsoft.com>
2017-10-30 13:23:06 +0200

    Imindstudio\Autoship

--- app/code/Imindstudio/Autoship/Block/Orders.php
+++ app/code/Imindstudio/Autoship/Block/Orders.php
@@ -4,34 +4,30 @@ namespace Imindstudio\Autoship\Block;
 
 class Orders extends \Magento\Sales\Block\Order\History
 {
-    private $orderCollectionFactory;
+    const PAGE_LIMIT = 10;
 
-    protected $_searchCriteria;
-    protected $_orderRepository;
+    protected $_request;
+    protected $_orderCurrency;
+    protected $_currencyFactory;
     protected $_collectionFactory;
     protected $_profileCollectionFactory;
-    protected $_profileRepositoryInterface;
-    protected $_profileOrderRepositoryInterface;
 
     public function __construct(
+        \Magento\Framework\App\Request\Http $request,
         \Magento\Sales\Model\Order\Config $orderConfig,
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory,
         \Magento\Framework\Data\CollectionFactory $collectionFactory,
-        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
-        \Magento\Framework\Api\SearchCriteriaBuilder $searchCriteria,
-        \Aheadworks\Sarp\Api\ProfileRepositoryInterface $profileRepositoryInterface,
         \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
-        \Aheadworks\Sarp\Api\ProfileOrderRepositoryInterface $profileOrderRepositoryInterface,
         \Aheadworks\Sarp\Model\ResourceModel\Profile\Order\CollectionFactory $profileCollectionFactory,
         array $data = []
     ) {
-        $this->_searchCriteria                  = $searchCriteria;
-        $this->_orderRepository                 = $orderRepository;
-        $this->_collectionFactory               = $collectionFactory;
-        $this->_profileCollectionFactory        = $profileCollectionFactory;
-        $this->_profileRepositoryInterface      = $profileRepositoryInterface;
-        $this->_profileOrderRepositoryInterface = $profileOrderRepositoryInterface;
+        $this->orders                    = null;
+        $this->_request                  = $request;
+        $this->_currencyFactory          = $currencyFactory;
+        $this->_collectionFactory        = $collectionFactory;
+        $this->_profileCollectionFactory = $profileCollectionFactory;
 
         parent::__construct(
             $context,
@@ -42,97 +38,113 @@ class Orders extends \Magento\Sales\Block\Order\History
         );
     }
 
-    public function setOrders($collection)
-    {
-        $this->orders = $collection;
-    }
-
-    public function getOrders()
-    {
-        if ($this->orders) {
-            $profileCollection = $this->_profileCollectionFactory->create();
-
-            $profileCollection->setCurPage(2);
-            $profileCollection->setPageSize(1);
-
-            foreach ($profileCollection as $profile) {
-                var_dump($profile->get());
-            }
-
-            return $this->orders;
-        } else {
-            return [];
-        }
-    }
-
     public function getPagerHtml()
     {
         return $this->getChildHtml('orders.pager');
     }
 
-    public function getOriginOrders()
+    public function getOrderCurrency($code)
     {
-        $orders = $this->_getNewCollection();
-        $profileOrders = $this->_getProfileOrders();
+        if ($this->_orderCurrency === null) {
+            $this->_orderCurrency = $this->_currencyFactory->create();
+        }
 
-        foreach ($profileOrders as $profileOrder) {
-            $orders->addItem($this->_orderRepository->get($profileOrder->getOrderId()));
-        };
-
-        return $orders;
+        return $this->_orderCurrency->load($code);
     }
 
-    public function getProfileOrders()
+    public function formatPrice($currencyCode, $price, $addBrackets = false)
     {
-        $orders = $this->_getNewCollection();
-        $profileOrders = $this->_getProfileOrders();
+        return $this->formatPricePrecision($currencyCode, $price, 2, $addBrackets);
+    }
 
-        foreach ($profileOrders as $profileOrder) {
-            $order = $this->_orderRepository->get($profileOrder->getOrderId());
-            $profile = $this->_profileRepositoryInterface->get($profileOrder->getProfileId());
-            $previousDate = $profile->getLastOrderDate();
-            $datePeriod = $profile->getBillingPeriod();
-            $datePeriodCount = intval($datePeriod);
+    public function getReorderUrl($order)
+    {
+        return $this->getUrl('sales/order/reorder', ['order_id' => $order->getEntityId()]);
+    }
 
-            if ($previousDate) {
-                $nextDate = date('Y-m-d',
-                    strtotime(
-                        $previousDate.' + '.($datePeriodCount ? $datePeriodCount : '1').' '.$datePeriod
-                    )
+    public function formatPricePrecision($currencyCode, $price, $precision, $addBrackets = false)
+    {
+        return $this->getOrderCurrency($currencyCode)
+            ->formatPrecision($price, $precision, [], true, $addBrackets);
+    }
+
+    public function getOrders()
+    {
+        if (!$this->orders) {
+            $page = $this->_request->getParam('p');
+            $profileCollection = $this->_profileCollectionFactory->create();
+
+            $profileCollection->setPageSize(self::PAGE_LIMIT)->setCurPage($page ? $page : 1);
+            $profileCollection->addFieldToSelect(['order_id', 'profile_id']);
+            $profileCollection->getSelect()->join('sales_order',
+                'sales_order.entity_id = main_table.order_id ',
+                [
+                    'status',
+                    'entity_id',
+                    'created_at',
+                    'customer_id',
+                    'increment_id',
+                    'order_currency_code',
+                    'shipping_address_id'
+                ]
+            );
+            $profileCollection->getSelect()->join('sales_order_address',
+                'sales_order_address.entity_id = sales_order.shipping_address_id ',
+                ['lastname', 'firstname']
+            );
+            $profileCollection->addFieldToFilter(
+                'sales_order.customer_id', $this->_customerSession->getCustomer()->getId()
+            );
+
+            if ($this->_request->getParam('type') == 'autoship') {
+                $profileCollection->getSelect()->join('aw_sarp_profile',
+                    'aw_sarp_profile.profile_id = main_table.profile_id ',
+                    ['billing_period', 'last_order_date']
                 );
+
+                foreach ($profileCollection as $profile) {
+                    $datePeriod = $profile->getBillingPeriod();
+                    $previousDate = $profile->getLastOrderDate();
+                    $datePeriodCount = intval($datePeriod);
+
+                    if ($previousDate) {
+                        $nextDate = date('Y-m-d',
+                            strtotime(
+                                $previousDate.' + '.($datePeriodCount ? $datePeriodCount : '1').' '.$datePeriod
+                            )
+                        );
+                    } else {
+                        $nextDate = null;
+                    }
+
+                    $profile->setNextShipDate($nextDate);
+                    $profile->setFrequency($profile->getBillingPeriod());
+                };
             } else {
-                $nextDate = null;
+                $this->setTemplate('Imindstudio_Autoship::history.phtml');
             }
 
-            $order->setNextShipDate($nextDate);
-            $order->setFrequency($profile->getBillingPeriod());
-            $order->setProfileId($profile->getProfileId());
-            $orders->addItem($order);
-        };
-
-        return $orders;
-    }
-
-    protected function _getNewCollection()
-    {
-        $collection = $this->_collectionFactory->create();
-
-        $collection->setCurPage(1);
-        $collection->setPageSize(1);
-
-        $collection->load();
-
-        return $collection;
-    }
-
-    protected function _getProfileOrders()
-    {
-        if (!($customerId = $this->_customerSession->getCustomerId())) {
-            return [];
-        } else {
-            return $this->_profileOrderRepositoryInterface->getList(
-                $this->_searchCriteria->addFilter('customer_id', $customerId)->create()
-            )->getItems();
+            $this->orders = $profileCollection->load();
         }
+
+        return $this->orders;
+    }
+
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+        if ($this->getOrders()) {
+            $pager = $this->getLayout()->createBlock(
+                'Magento\Theme\Block\Html\Pager',
+                'autoship.orders.pager'
+            )
+                ->setLimit(self::PAGE_LIMIT)
+                ->setCollection(
+                    $this->getOrders()
+                );
+
+            $this->setChild('orders.pager', $pager);
+        }
+        return $this;
     }
 }
--- app/code/Imindstudio/Autoship/Controller/Customer/Orders.php
+++ app/code/Imindstudio/Autoship/Controller/Customer/Orders.php
@@ -6,27 +6,12 @@ class Orders extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        $request = $this->getRequest();
-        $layout = $this->_view->loadLayout();
-        $ordersBlock = $layout->getLayout()->getBlock('orders');
+        $this->_view->loadLayout();
 
-        if ($request->getParam('type') == 'origin_order') {
-            $collection = $ordersBlock->getOriginOrders();
-
-            $ordersBlock->setTemplate('Magento_Sales::order/history.phtml');
-        } else {
-            $collection = $ordersBlock->getProfileOrders();
-        }
-
-        if ($collection) {
-            $ordersBlock->setOrders($collection);
-
-            $pager = $layout->getLayout()->createBlock(
-                'Magento\Theme\Block\Html\Pager',
-                'orders.pager'
-            )->setCollection($ordersBlock->getOrders());
-
-            $ordersBlock->setChild('orders.pager', $pager);
+        if ($this->getRequest()->getParam('type') != 'autoship') {
+            $this->_view->getLayout()
+                ->getBlock('page.main.title')
+                ->setPageTitle(__('YOUR ORDER HISTORY'));
         }
 
 		$this->_view->renderLayout();
